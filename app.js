40cec684c061a1b57dd279a54696da087c45c567
masonmike2001 <masonmike2001@gmail.com>
2022-11-05 12:51:43 -0400

    add proper grid format, book integration

---
+++ app.js
@@ -0,0 +1,23 @@
+let myLibrary = [];
+
+function Book() 
+{
+    //constructor
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+}
+
+function addBookToLibrary()
+{
+    
+}
+
+function displayBooks()
+{
+for (i = 0; i < myLibrary.length; i++) {
+    //display the book in DOM grid with pertinent info
+
+}
+}
